fffdc1b5c289c62393c8acb33f917ebe14f0daa1
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-09-19 00:29:33 -0500

    183. Terminar el Product Card

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,5 +1,12 @@
-import { FC } from 'react';
-import { Card, CardActionArea, CardMedia, Grid } from '@mui/material';
+import { FC, useMemo, useState } from 'react';
+import {
+	Box,
+	Card,
+	CardActionArea,
+	CardMedia,
+	Grid,
+	Typography
+} from '@mui/material';
 import { IProduct } from '../../interfaces';
 
 interface Props {
@@ -7,19 +14,37 @@ interface Props {
 }
 
 export const ProductCard: FC<Props> = ({
-	product: { slug, images, title }
+	product: { images, title, price }
 }) => {
+	const [isHovered, setIsHovered] = useState(false);
+
+	const productImage = useMemo(() => {
+		return isHovered ? `products/${images[1]}` : `products/${images[0]}`;
+	}, [isHovered, images]);
+
 	return (
-		<Grid item xs={6} sm={4} key={slug}>
+		<Grid
+			item
+			xs={6}
+			sm={4}
+			onMouseEnter={() => setIsHovered(true)}
+			onMouseLeave={() => setIsHovered(false)}
+		>
 			<Card>
 				<CardActionArea>
 					<CardMedia
 						component="img"
-						image={`products/${images[0]}`}
+						className="fadeIn"
+						image={productImage}
 						alt={title}
 					/>
 				</CardActionArea>
 			</Card>
+
+			<Box sx={{ mt: 1 }} className="fadeIn">
+				<Typography fontWeight={700}>{title}</Typography>
+				<Typography fontWeight={500}>{`$${price}`}</Typography>
+			</Box>
 		</Grid>
 	);
 };
