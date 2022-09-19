1893be6313c6dfbf5bf98af2c3741391a5b1020e
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-09-19 00:50:03 -0500

    184. Pantalla de producto

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,10 +1,12 @@
 import { FC, useMemo, useState } from 'react';
+import NextLink from 'next/link';
 import {
 	Box,
 	Card,
 	CardActionArea,
 	CardMedia,
 	Grid,
+	Link,
 	Typography
 } from '@mui/material';
 import { IProduct } from '../../interfaces';
@@ -31,14 +33,18 @@ export const ProductCard: FC<Props> = ({
 			onMouseLeave={() => setIsHovered(false)}
 		>
 			<Card>
-				<CardActionArea>
-					<CardMedia
-						component="img"
-						className="fadeIn"
-						image={productImage}
-						alt={title}
-					/>
-				</CardActionArea>
+				<NextLink href="/product/slug" passHref prefetch={false}>
+					<Link>
+						<CardActionArea>
+							<CardMedia
+								component="img"
+								className="fadeIn"
+								image={productImage}
+								alt={title}
+							/>
+						</CardActionArea>
+					</Link>
+				</NextLink>
 			</Card>
 
 			<Box sx={{ mt: 1 }} className="fadeIn">
