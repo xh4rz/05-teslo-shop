84f665559eadcd0125074eff39419f2a8245c60c
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-09-23 00:04:39 -0500

    185. Slideshow de las imágenes

--- pages/product/slug.tsx
+++ pages/product/slug.tsx
@@ -2,6 +2,7 @@ import { Button, Chip, Grid, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import { ShopLayout } from '../../components/layouts';
 import { initialData } from '../../database/products';
+import { ProductSlideshow } from '../../components/products';
 
 const product = initialData.products[0];
 
@@ -11,6 +12,7 @@ const ProductPage = () => {
 			<Grid container spacing={3}>
 				<Grid item xs={12} sm={7}>
 					{/* Slideshow */}
+					<ProductSlideshow images={product.images} />
 				</Grid>
 				<Grid item xs={12} sm={5}>
 					<Box display="flex" flexDirection="column">
