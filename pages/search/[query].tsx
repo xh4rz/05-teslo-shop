c0ad02beeb96d2c32cdd78287656eea367960905
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-12-01 00:01:35 -0500

    230. Realizar la búsqueda mediante SSR

--- pages/search/[query].tsx
+++ pages/search/[query].tsx
@@ -1,12 +1,14 @@
-import type { NextPage } from 'next';
+import type { NextPage, GetServerSideProps } from 'next';
 import { ShopLayout } from '../../components/layouts';
 import { Typography } from '@mui/material';
 import { ProductList } from '../../components/products';
-import { useProducts } from '../../hooks';
-import { FullScreenLoading } from '../../components/ui';
+import { dbProducts } from '../../database';
+import { IProduct } from '../../interfaces';
+interface Props {
+	products: IProduct[];
+}
 
-const SearchPage: NextPage = () => {
-	const { products, isLoading } = useProducts('/search/Haha');
+const SearchPage: NextPage<Props> = ({ products }) => {
 	return (
 		<ShopLayout
 			title={'Teslo-Shop - Search'}
@@ -19,9 +21,35 @@ const SearchPage: NextPage = () => {
 				ABC --- 123
 			</Typography>
 
-			{isLoading ? <FullScreenLoading /> : <ProductList products={products} />}
+			<ProductList products={products} />
 		</ShopLayout>
 	);
+};
+
+// You should use getServerSideProps when:
+// - Only if you need to pre-render a page whose data must be fetched at request time
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+	const { query = '' } = params as { query: string };
+
+	if (query.length === 0) {
+		return {
+			redirect: {
+				destination: '/',
+				permanent: true
+			}
+		};
+	}
+
+	let products = await dbProducts.getProductsByTerm(query);
+
+	// TODO: retornar otros productos
+
+	return {
+		props: {
+			products
+		}
+	};
 };
 
 export default SearchPage;
