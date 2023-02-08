04543230b7ea69c065dcd157c3d8af22b49e75a8
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2023-02-07 22:46:50 -0500

    263. Crear JWT - JsonWebToken

---
+++ utils/jwt.ts
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+
+export const signToken = (_id: string, email: string) => {
+	if (!process.env.JWT_SECRET_SEED) {
+		throw new Error('No hay semilla de JWT - Revisar variables de entorno');
+	}
+
+	return jwt.sign(
+		// Payload
+		{ _id, email },
+
+		// Seed
+		process.env.JWT_SECRET_SEED,
+
+		// Opciones
+		{ expiresIn: '30d' }
+	);
+};
