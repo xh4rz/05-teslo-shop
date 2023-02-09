b18dc44203ee2833374284449465c2a8a2b283bf
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2023-02-09 01:07:28 -0500

    270. Mostrar mensaje de error en pantalla

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,6 +1,16 @@
+import { useState } from 'react';
 import NextLink from 'next/link';
 import { AuthLayout } from '../../components/layouts';
-import { Box, Button, Grid, Link, TextField, Typography } from '@mui/material';
+import {
+	Box,
+	Button,
+	Chip,
+	Grid,
+	Link,
+	TextField,
+	Typography
+} from '@mui/material';
+import { ErrorOutline } from '@mui/icons-material';
 import { useForm } from 'react-hook-form';
 import { validations } from '../../utils';
 import { tesloApi } from '../../api';
@@ -17,7 +27,11 @@ const LoginPage = () => {
 		formState: { errors }
 	} = useForm<FormData>();
 
+	const [showError, setShowError] = useState(false);
+
 	const onLoginUser = async ({ email, password }: FormData) => {
+		setShowError(false);
+
 		try {
 			const { data } = await tesloApi.post('/user/login', {
 				email,
@@ -29,7 +43,13 @@ const LoginPage = () => {
 			console.log({ token, user });
 		} catch (error) {
 			console.log('Error en las credenciales');
+			setShowError(true);
+			setTimeout(() => {
+				setShowError(false);
+			}, 3000);
 		}
+
+		// TODO: navegar a la pantalla que el usuario estaba
 	};
 
 	return (
@@ -41,6 +61,13 @@ const LoginPage = () => {
 							<Typography variant="h1" component="h1">
 								Iniciar Sesión
 							</Typography>
+							<Chip
+								label="No reconocemos ese usuario / contraseña"
+								color="error"
+								icon={<ErrorOutline />}
+								className="fadeIn"
+								sx={{ display: showError ? 'flex' : 'none' }}
+							/>
 						</Grid>
 						<Grid item xs={12}>
 							<TextField
