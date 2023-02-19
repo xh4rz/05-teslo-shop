0fb18e3812058ef425497f666ad96f070f10557a
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2023-02-18 21:12:26 -0500

    281. Estilo condicional en el menú lateral

--- components/ui/SideMenu.tsx
+++ components/ui/SideMenu.tsx
@@ -24,13 +24,15 @@ import {
 	VpnKeyOutlined
 } from '@mui/icons-material';
 import { useContext, useState } from 'react';
-import { UiContext } from '../../context';
+import { AuthContext, UiContext } from '../../context';
 import { useRouter } from 'next/router';
 
 export const SideMenu = () => {
 	const router = useRouter();
 
 	const { isMenuOpen, toggleSideMenu } = useContext(UiContext);
+
+	const { user, isLoggedIn } = useContext(AuthContext);
 
 	const [searchTerm, setsearchTerm] = useState('');
 
@@ -72,19 +74,23 @@ export const SideMenu = () => {
 						/>
 					</ListItem>
 
-					<ListItem button>
-						<ListItemIcon>
-							<AccountCircleOutlined />
-						</ListItemIcon>
-						<ListItemText primary={'Perfil'} />
-					</ListItem>
+					{isLoggedIn && (
+						<>
+							<ListItem button>
+								<ListItemIcon>
+									<AccountCircleOutlined />
+								</ListItemIcon>
+								<ListItemText primary={'Perfil'} />
+							</ListItem>
 
-					<ListItem button>
-						<ListItemIcon>
-							<ConfirmationNumberOutlined />
-						</ListItemIcon>
-						<ListItemText primary={'Mis Ordenes'} />
-					</ListItem>
+							<ListItem button>
+								<ListItemIcon>
+									<ConfirmationNumberOutlined />
+								</ListItemIcon>
+								<ListItemText primary={'Mis Ordenes'} />
+							</ListItem>
+						</>
+					)}
 
 					<ListItem
 						button
@@ -119,43 +125,50 @@ export const SideMenu = () => {
 						<ListItemText primary={'Niños'} />
 					</ListItem>
 
-					<ListItem button>
-						<ListItemIcon>
-							<VpnKeyOutlined />
-						</ListItemIcon>
-						<ListItemText primary={'Ingresar'} />
-					</ListItem>
-
-					<ListItem button>
-						<ListItemIcon>
-							<LoginOutlined />
-						</ListItemIcon>
-						<ListItemText primary={'Salir'} />
-					</ListItem>
+					{isLoggedIn ? (
+						<ListItem button>
+							<ListItemIcon>
+								<LoginOutlined />
+							</ListItemIcon>
+							<ListItemText primary={'Salir'} />
+						</ListItem>
+					) : (
+						<ListItem button>
+							<ListItemIcon>
+								<VpnKeyOutlined />
+							</ListItemIcon>
+							<ListItemText primary={'Ingresar'} />
+						</ListItem>
+					)}
 
 					{/* Admin */}
-					<Divider />
-					<ListSubheader>Admin Panel</ListSubheader>
 
-					<ListItem button>
-						<ListItemIcon>
-							<CategoryOutlined />
-						</ListItemIcon>
-						<ListItemText primary={'Productos'} />
-					</ListItem>
-					<ListItem button>
-						<ListItemIcon>
-							<ConfirmationNumberOutlined />
-						</ListItemIcon>
-						<ListItemText primary={'Ordenes'} />
-					</ListItem>
+					{user?.role === 'admin' && (
+						<>
+							<Divider />
+							<ListSubheader>Admin Panel</ListSubheader>
 
-					<ListItem button>
-						<ListItemIcon>
-							<AdminPanelSettings />
-						</ListItemIcon>
-						<ListItemText primary={'Usuarios'} />
-					</ListItem>
+							<ListItem button>
+								<ListItemIcon>
+									<CategoryOutlined />
+								</ListItemIcon>
+								<ListItemText primary={'Productos'} />
+							</ListItem>
+							<ListItem button>
+								<ListItemIcon>
+									<ConfirmationNumberOutlined />
+								</ListItemIcon>
+								<ListItemText primary={'Ordenes'} />
+							</ListItem>
+
+							<ListItem button>
+								<ListItemIcon>
+									<AdminPanelSettings />
+								</ListItemIcon>
+								<ListItemText primary={'Usuarios'} />
+							</ListItem>
+						</>
+					)}
 				</List>
 			</Box>
 		</Drawer>
