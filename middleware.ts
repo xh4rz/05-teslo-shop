316400dcd87597f088cc5c4003065e688c7a6cd6
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2023-04-12 00:04:45 -0500

    308. Next 13 - Middleware con Next Auth

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,56 @@
 // middleware.ts
-import { NextResponse, type NextRequest } from 'next/server';
-import * as jose from 'jose';
+// import { NextResponse, type NextRequest } from 'next/server';
+// import * as jose from 'jose';
 
+// export async function middleware(req: NextRequest) {
+// 	const previousPage = req.nextUrl.pathname;
+
+// 	if (previousPage.startsWith('/checkout')) {
+// 		const token = req.cookies.get('token') || '';
+
+// 		try {
+// 			await jose.jwtVerify(
+// 				token,
+// 				new TextEncoder().encode(process.env.JWT_SECRET_SEED)
+// 			);
+// 			return NextResponse.next();
+// 		} catch (error) {
+// 			return NextResponse.redirect(
+// 				new URL(`/auth/login?p=${previousPage}`, req.url)
+// 			);
+// 		}
+// 	}
+// }
+
+// export const config = {
+// 	matcher: ['/checkout/:path*']
+// };
+
+// middleware.ts
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+
+// This function can be marked `async` if using `await` inside
 export async function middleware(req: NextRequest) {
-	const previousPage = req.nextUrl.pathname;
+	const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
-	if (previousPage.startsWith('/checkout')) {
-		const token = req.cookies.get('token') || '';
+	// Información útil sobre el usuario
 
-		try {
-			await jose.jwtVerify(
-				token,
-				new TextEncoder().encode(process.env.JWT_SECRET_SEED)
-			);
-			return NextResponse.next();
-		} catch (error) {
-			return NextResponse.redirect(
-				new URL(`/auth/login?p=${previousPage}`, req.url)
-			);
-		}
+	if (!session) {
+		const requestedPage = req.nextUrl.pathname;
+		const url = req.nextUrl.clone();
+		url.pathname = '/auth/login';
+		url.search = `p=${requestedPage}`;
+
+		return NextResponse.redirect(url);
 	}
+
+	// return NextResponse.redirect(new URL('/about-2', request.url));
+	return NextResponse.next();
 }
 
+// See "Matching Paths" below to learn more
 export const config = {
-	matcher: ['/checkout/:path*']
+	matcher: ['/checkout/address', '/checkout/summary']
 };
