51c0c72c7a86162223ffd1b2815ec619b2681c4a
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2023-06-01 03:40:43 -0500

    316. Continuación de la sección

--- context/auth/AuthProvider.tsx
+++ context/auth/AuthProvider.tsx
@@ -26,7 +26,6 @@ export const AuthProvider: FC<PropsWithChildren> = ({ children }) => {
 
 	useEffect(() => {
 		if (status === 'authenticated') {
-			console.log({ user: data.user });
 			dispatch({ type: '[Auth] - Login', payload: data?.user as IUser });
 		}
 	}, [status, data]);
