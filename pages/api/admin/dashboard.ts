b69677102584b9e64414552ae7ad9c57c849b709
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2023-07-16 00:48:34 -0500

    352. Resolución de la tarea

--- pages/api/admin/dashboard.ts
+++ pages/api/admin/dashboard.ts
@@ -16,30 +16,46 @@ export default async function handlder(
 	req: NextApiRequest,
 	res: NextApiResponse<Data>
 ) {
+	// const numberOfOrders = await Order.count();
+
+	// const paidOrders = await Order.find({ isPaid: true }).count();
+
+	// const numberOfClients = await User.find({ role: 'client' }).count();
+
+	// const numberOfProducts = await Product.count();
+
+	// const productsWithNoInventory = await Product.find({ inStock: 0 }).count();
+
+	// const lowInventory = await Product.find({
+	// 	inStock: { $lte: 10 }
+	// }).count();
+
 	await db.connect();
 
-	const numberOfOrders = await Order.count();
-
-	const paidOrders = await Order.find({ isPaid: true }).count();
-
-	const notPaidOrders = await Order.find({ isPaid: false }).count();
-
-	const numberOfClients = await User.find({ role: 'client' }).count();
-
-	const numberOfProducts = await Product.count();
-
-	const productsWithNoInventory = await Product.find({ inStock: 0 }).count();
-
-	const lowInventory = await Product.find({
-		inStock: { $gt: 0, $lt: 10 }
-	}).count();
+	const [
+		numberOfOrders,
+		paidOrders,
+		numberOfClients,
+		numberOfProducts,
+		productsWithNoInventory,
+		lowInventory
+	] = await Promise.all([
+		Order.count(),
+		Order.find({ isPaid: true }).count(),
+		User.find({ role: 'client' }).count(),
+		Product.count(),
+		Product.find({ inStock: 0 }).count(),
+		Product.find({
+			inStock: { $lte: 10 }
+		}).count()
+	]);
 
 	await db.disconnect();
 
 	res.status(200).json({
 		numberOfOrders,
 		paidOrders,
-		notPaidOrders,
+		notPaidOrders: numberOfOrders - paidOrders,
 		numberOfClients,
 		numberOfProducts,
 		productsWithNoInventory,
