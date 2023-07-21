965b3cd4116cffebf336c80481388f3f12718e39
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2023-07-20 23:11:05 -0500

    355. REST - Mantenimiento de usuarios

---
+++ pages/api/admin/users.ts
@@ -0,0 +1,70 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { db } from '../../../database';
+import { User } from '../../../models';
+import { IUser } from '../../../interfaces';
+import { isValidObjectId } from 'mongoose';
+
+type Data =
+	| {
+			message: string;
+	  }
+	| IUser[];
+
+export default function handlder(
+	req: NextApiRequest,
+	res: NextApiResponse<Data>
+) {
+	switch (req.method) {
+		case 'GET':
+			return getUsers(req, res);
+
+		case 'PUT':
+			return updateUser(req, res);
+		default:
+			return res.status(400).json({ message: 'Bad request' });
+	}
+}
+const getUsers = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+	await db.connect();
+
+	const users = await User.find().select('-password').lean();
+
+	await db.disconnect();
+
+	return res.status(200).json(users);
+};
+
+const updateUser = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+	const { userId = '', role = '' } = req.body;
+
+	if (!isValidObjectId(userId)) {
+		return res.status(400).json({ message: 'No existe usuario por ese id' });
+	}
+
+	const validRoles = ['admin', 'super-user', 'SEO'];
+
+	if (!validRoles.includes(role)) {
+		return res
+			.status(400)
+			.json({ message: 'Rol no permitido: ' + validRoles.join(', ') });
+	}
+
+	await db.connect();
+
+	const user = await User.findById(userId);
+
+	if (!user) {
+		await db.disconnect();
+		return res
+			.status(404)
+			.json({ message: 'Usuario no encontrado: ' + userId });
+	}
+
+	user.role = role;
+
+	await user.save();
+
+	await db.disconnect();
+
+	return res.status(200).json({ message: 'Usuario actualizado' });
+};
