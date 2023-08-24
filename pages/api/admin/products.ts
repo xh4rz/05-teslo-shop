2dfc47a0a116d3ecb285ff7f00d9e4b913f9c6ee
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2023-08-24 02:13:14 -0500

    378. REST - Actualizar Producto

--- pages/api/admin/products.ts
+++ pages/api/admin/products.ts
@@ -2,12 +2,14 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { IProduct } from '../../../interfaces';
 import { db } from '../../../database';
 import { Product } from '../../../models';
+import { isValidObjectId } from 'mongoose';
 
 type Data =
 	| {
 			message: string;
 	  }
-	| IProduct[];
+	| IProduct[]
+	| IProduct;
 
 export default function hanlder(
 	req: NextApiRequest,
@@ -18,10 +20,9 @@ export default function hanlder(
 			return getProducts(req, res);
 
 		case 'PUT':
-			break;
+			return updateProducts(req, res);
 
 		case 'POST':
-			break;
 
 		default:
 			return res.status(400).json({
@@ -40,4 +41,54 @@ const getProducts = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 	// TODO: Tendremos que actualizar las imagenes
 
 	res.status(200).json(products);
+};
+
+const updateProducts = async (
+	req: NextApiRequest,
+	res: NextApiResponse<Data>
+) => {
+	const { _id = '', images = [] } = req.body as IProduct;
+
+	if (!isValidObjectId(_id)) {
+		return res.status(400).json({
+			message: 'El id del producto no es válido'
+		});
+	}
+
+	if (images.length <= 2) {
+		return res.status(400).json({
+			message: 'Es necesario al menos 2 imágenes'
+		});
+	}
+
+	// TODO: posiblemente tendremos un localhost:3000/products/asdasd.jpg
+
+	try {
+		await db.connect();
+
+		const product = await Product.findById(_id);
+
+		if (!product) {
+			await db.disconnect();
+			return res.status(400).json({
+				message: 'No existe un producto con ese ID'
+			});
+		}
+
+		// TODO: eliminar fotos en Cloudinary
+
+		await product.update(req.body);
+
+		await db.disconnect();
+
+		return res.status(200).json(product);
+	} catch (error) {
+		console.log(error);
+
+		await db.disconnect();
+
+		return res.status(400).json({
+			message: 'Revisar la consola del servidor'
+		});
+	}
 };
