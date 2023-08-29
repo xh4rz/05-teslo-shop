4830bad095c485b5dfe2c045eaca8aa2ddfed148
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2023-08-29 00:36:44 -0500

    update test

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -171,26 +171,26 @@ const LoginPage = () => {
 // You should use getServerSideProps when:
 // - Only if you need to pre-render a page whose data must be fetched at request time
 
-export const getServerSideProps: GetServerSideProps = async ({
-	req,
-	query
-}) => {
-	const session = await getSession({ req });
+// export const getServerSideProps: GetServerSideProps = async ({
+// 	req,
+// 	query
+// }) => {
+// 	const session = await getSession({ req });
 
-	const { p = '/' } = query;
+// 	const { p = '/' } = query;
 
-	if (session) {
-		return {
-			redirect: {
-				destination: p.toString(),
-				permanent: false
-			}
-		};
-	}
+// 	if (session) {
+// 		return {
+// 			redirect: {
+// 				destination: p.toString(),
+// 				permanent: false
+// 			}
+// 		};
+// 	}
 
-	return {
-		props: {}
-	};
-};
+// 	return {
+// 		props: {}
+// 	};
+// };
 
 export default LoginPage;
